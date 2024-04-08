1108931cfc387afd15ed4a8628df70e766495574
mtaborda@primary.com.ar <mtaborda@primary.com.ar>
2024-04-08 09:40:33 +0200

    update to .net 8

---
+++ TestNinjaNet8/Mocking/BookingRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.Mocking
+{
+    public interface IBookingRepository
+    {
+        IQueryable<Booking> GetActiveBookings(int? excludedBookingId);
+    }
+
+    public class BookingRepository : IBookingRepository
+    {
+
+        public BookingRepository()
+        {
+
+        }
+
+        public IQueryable<Booking> GetActiveBookings(int? excludedBookingId)
+        {
+            var unitOfWork = new UnitOfWork();
+            var bookings =
+                unitOfWork.Query<Booking>()
+                    .Where(
+                        b => (b.Id != excludedBookingId || excludedBookingId == null) && b.Status != "Cancelled");
+
+            return bookings;
+        }
+    }
+}
---
+++ TestNinjaNet8/Mocking/VideoService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using Microsoft.EntityFrameworkCore;
+
+using Newtonsoft.Json;
+
+namespace TestNinja.Mocking
+{
+    public class VideoService
+    {
+        private IFileReader _fileReader;
+        private IVideosRepository _videosRepository;
+
+        public VideoService(IFileReader fileReader = null, IVideosRepository videosReader = null)
+        {
+            _fileReader = fileReader ?? new FileReader();
+            _videosRepository = videosReader ?? new VideosRepository();
+        }
+
+        public string ReadVideoTitle()
+        {
+            var str = _fileReader.Read("video.txt");
+            var video = JsonConvert.DeserializeObject<Video>(str);
+            if (video == null)
+                return "Error parsing the video.";
+            return video.Title;
+        }
+
+        public string GetUnprocessedVideosAsCsv()
+        {
+            var videoIds = new List<int>();
+            
+              
+            foreach (var v in _videosRepository.GetUnprocessedVideos())
+                videoIds.Add(v.Id);
+
+            return String.Join(",", videoIds);
+
+        }
+
+
+
+    }
+
+    public class Video
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsProcessed { get; set; }
+    }
+
+    public class VideoContext : DbContext
+    {
+        public DbSet<Video> Videos { get; set; }
+    }
+}
